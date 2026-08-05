d243f3efd155c255deb85362f6842ff3bd597a51
DivyanshuSharmaCoader <divyasharma1b18@gmail.com>
2026-08-06 02:48:53 +0530

    Containerize application with Docker Compose and Celery

--- migrations/env.py
+++ migrations/env.py
@@ -14,8 +14,7 @@ from sqlmodel import SQLModel
 config = context.config
 config.set_main_option(
     "sqlalchemy.url", 
-    #db_settings.POSTGRES_URL
-    "postgresql+asyncpg://postgres:#Divyanshu_108@localhost:5433/fastship"
+    db_settings.POSTGRES_URL
     )
 
 # Interpret the config file for Python logging.

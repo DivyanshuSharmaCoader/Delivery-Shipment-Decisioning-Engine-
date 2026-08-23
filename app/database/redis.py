9ca55c356adbc168d3eb73188fd25a96ed9bb400
DivyanshuSharmaCoader <divyasharma1b18@gmail.com>
2026-08-24 01:25:18 +0530

    update

--- app/database/redis.py
+++ app/database/redis.py
@@ -1,27 +1,26 @@
 from uuid import UUID
 
 from redis.asyncio import Redis
+
 from app.config import db_settings
 
 
-_token_blacklist = Redis(
-    host=db_settings.REDIS_HOST,
-    port=db_settings.REDIS_PORT,
-    db=0,
+_token_blacklist = Redis.from_url(
+    db_settings.REDIS_URL(0),
 )
-_shipment_verification_codes = Redis(
-    host=db_settings.REDIS_HOST,
-    port=db_settings.REDIS_PORT,
-    db=1,
+
+_shipment_verification_codes = Redis.from_url(
+    db_settings.REDIS_URL(1),
     decode_responses=True,
 )
+
 
 async def add_jti_to_blacklist(jti: str):
     try:
         await _token_blacklist.set(jti, "blacklisted")
     except Exception:
         pass
-    
+
 
 async def is_jti_blacklisted(jti: str) -> bool:
     try:
@@ -29,11 +28,13 @@ async def is_jti_blacklisted(jti: str) -> bool:
     except Exception:
         return False
 
+
 async def add_shipment_verification_code(id: UUID, code: int):
     try:
         await _shipment_verification_codes.set(str(id), code)
     except Exception:
         pass
+
 
 async def get_shipment_verification_code(id: UUID):
     try:
